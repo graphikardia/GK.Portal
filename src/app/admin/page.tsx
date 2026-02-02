9d29eb74a22bb6908922f0994d663a352fe053bd
notRyuk <amadeusrobot.notryuk@gmail.com>
2026-02-02 13:47:17 +0530

    test: logs

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -34,11 +34,11 @@ export default function AdminPortal() {
     const cleanOtp = otp.trim();
 
     try {
-      console.log(import.meta.env.VITE_OTPLIB_SECRET, cleanOtp)
       const otpVerification = await verifyOTP({
         secret: import.meta.env.VITE_OTPLIB_SECRET,
         token: cleanOtp
       })
+      console.log(otpVerification.valid)
       if (cleanOtp.length === 6 && otpVerification.valid) {
         setIsAuth(true);
         toast.success("Login Successful!")
